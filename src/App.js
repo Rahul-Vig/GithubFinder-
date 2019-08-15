e7ed22c6842b4a22860e093097717287bffabd22
Rahul Vig <rahvee51395@gmail.com>
2019-08-14 21:41:59 -0400

    added github icon logo and created navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Fragment, Component } from "react";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 // function App() {
@@ -9,14 +10,46 @@ import "./App.css";
 //   );
 // }
 
-class App extends React.Component {
+//You can put any js expression in the {} when rendering html elements. {name} is one example,
+//can also do {1+1} and it will evaluate on the actual webpage as 2.
+//other examples: {name.toUpperCase()}
+
+//Can use expressions in JSX and pass functions, variables etc.
+
+class App extends Component {
+  //method part of class to be passed as jsx
+  foo = () => "Bars";
   render() {
+    const name = "John Doe";
+    const loading = false;
+    const showName = true;
+
+    // if (loading) {
+    //   return <h4>Loading....</h4>;
+    // }
+
     return (
       <div className="App">
-        <h1> Hello from React </h1>
+        {" "}
+        <Navbar />
       </div>
     );
-  } //render is a lifecycle method, it runs when the component has loaded.
-}
+
+    // return (
+    //   <Fragment>
+    //     <h1> Welcome to My App </h1>
+    //     {loading ? (
+    //       <h4>Loading...</h4>
+    //     ) : (
+    //       <h1>
+    //         Hello {showName ? name : null} <br />{" "}
+    //         {/*You can also just do showName && name*/}
+    //         {"There are many " + this.foo()}
+    //       </h1>
+    //     )}
+    //   </Fragment>
+    // );
+  }
+} //render is a lifecycle method, it runs when the component has loaded.
 
 export default App;
